eeeb117ea7cef81102e5117e1dcd52042eee2ccf
YousifMHelal <yousifM.Helal@gmail.com>
2024-09-04 00:27:21 +0300

    fix

--- src/components/Shared/SearchResult.tsx
+++ src/components/Shared/SearchResult.tsx
@@ -4,7 +4,7 @@ import Loader from "./Loader";
 
 type SearchResultsProps = {
   isSearchFetching: boolean;
-  searchedPosts: Models.Document[];
+  searchedPosts: Models.Document[]; // Correct type: array of Documents
 };
 
 const SearchResult = ({
@@ -13,8 +13,10 @@ const SearchResult = ({
 }: SearchResultsProps) => {
   if (isSearchFetching) return <Loader />;
 
-  if (searchedPosts && searchedPosts.documents.length > 0)
-    return <GridPostList posts={searchedPosts.documents} />;
+  // Directly use searchedPosts as an array
+  if (searchedPosts.length > 0) {
+    return <GridPostList posts={searchedPosts} />;
+  }
 
   return (
     <p className="text-light-4 mt-10 text-center w-full">No results found</p>
